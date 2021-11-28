2c2c54f5da6763c9d13dc7d423213d6d46369d98
Samnas <samucasnascimento@hotmail.com>
2021-11-28 17:32:41 -0300

    Learning EX 3

--- Fundamentos do Desenvolvimento Web/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/5.2 - JavaScript - Trabalhando com elementos/learning-exercises.js
+++ Fundamentos do Desenvolvimento Web/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/5.2 - JavaScript - Trabalhando com elementos/learning-exercises.js
@@ -1,12 +1,19 @@
 // Acesse o elemento elementoOndeVoceEsta .
-const ondeVoceEsta = document.getElementById('elementoOndeVoceEsta');
-console.log(ondeVoceEsta);
+const ondeVoceEsta = document.querySelector('#elementoOndeVoceEsta');
 // Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
 ondeVoceEsta.parentNode.style.color = "blue";
-console.log(pai.style.color);
+
 // Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
+ondeVoceEsta.firstElementChild.innerHTML = "OK";
+
 // Acesse o primeiroFilho a partir de pai .
+const pai = document.querySelector('#pai');
+pai.firstElementChild;
+
 // Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta .
+ondeVoceEsta.previousElementSibling;
+
 // Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta .
+console.log(ondeVoceEsta.nextSibling);
 // Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta .
 // Agora acesse o terceiroFilho a partir de pai .
