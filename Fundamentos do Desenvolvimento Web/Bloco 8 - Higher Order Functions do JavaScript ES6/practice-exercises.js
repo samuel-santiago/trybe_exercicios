bd0fc374dac5cd9b8cfc29aa998475bccb238c7a
Samnas <samucasnascimento@hotmail.com>
2021-12-19 19:57:35 -0300

    Practice-Exercises Ex1

---
+++ Fundamentos do Desenvolvimento Web/Bloco 8 - Higher Order Functions do JavaScript ES6/practice-exercises.js
@@ -0,0 +1,19 @@
+// 1 - Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada. Passe sua funçãocomo parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id . A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com .
+
+const newEmployees = (callback) => {
+  const employees = {
+    id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
+
+const createObj = (fullName) => {
+  const object = {
+    nomeCompleto: fullName,
+    email: `${(fullName).toLowerCase().replace(' ', '_')}@trybe.com`
+  }
+  return object;
+}
+console.log(newEmployees(createObj));
