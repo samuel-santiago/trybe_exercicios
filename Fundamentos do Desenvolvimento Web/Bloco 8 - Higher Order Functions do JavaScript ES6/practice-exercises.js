9b1df498721f99db9a0dc168494bc06e7b17c83d
Samnas <samucasnascimento@hotmail.com>
2021-12-19 20:39:21 -0300

    Practice-Exercises Ex2

--- Fundamentos do Desenvolvimento Web/Bloco 8 - Higher Order Functions do JavaScript ES6/practice-exercises.js
+++ Fundamentos do Desenvolvimento Web/Bloco 8 - Higher Order Functions do JavaScript ES6/practice-exercises.js
@@ -17,3 +17,14 @@ const createObj = (fullName) => {
   return object;
 }
 console.log(newEmployees(createObj));
+
+// 2 - Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
+
+const sweepstake = (number, callback1) => {
+  const drawnNumber = (Math.floor(Math.random() * 5 + 1));
+  return callback1(drawnNumber, number) ? 'Parabéns você ganhou':'Tente novamente';
+}
+
+const isEqual = (number, winner) => number === winner;
+
+sweepstake(1, isEqual);
