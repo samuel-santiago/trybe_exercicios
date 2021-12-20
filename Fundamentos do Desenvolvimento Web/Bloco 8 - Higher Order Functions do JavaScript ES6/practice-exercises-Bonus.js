e478771000a2ff65f39de8df305b02105e0cb882
Samnas <samucasnascimento@hotmail.com>
2021-12-19 22:20:33 -0300

    Practice-Exercises-Bonus Ex1

---
+++ Fundamentos do Desenvolvimento Web/Bloco 8 - Higher Order Functions do JavaScript ES6/practice-exercises-Bonus.js
@@ -0,0 +1,40 @@
+// Parte I - Game Actions Simulator
+// Nestes exercícios você irá implementar HOFs que simulam um turno de batalha em um jogo. Você irá criar funções que calculam dano, atualizam status, e ao final, retornam os resultados da rodada.
+// Para os próximos exercícios copie o código abaixo.
+
+const mage = {
+  healthPoints: 130,
+  intelligence: 45,
+  mana: 125,
+  damage: undefined,
+};
+
+const warrior = {
+  healthPoints: 200,
+  strength: 30,
+  weaponDmg: 2,
+  damage: undefined,
+};
+
+const dragon = {
+  healthPoints: 350,
+  strength: 50,
+  damage: undefined,
+};
+
+const battleMembers = { mage, warrior, dragon };
+
+// 1 - Crie uma função que retorna o dano do dragão.
+// O dano será um número aleatório entre 15 (dano mínimo) e o valor do atributo strength (dano máximo).
+
+
+  const dragonDamage = () => Math.random() * (dragon.strength - 15)  + 15;
+  console.log(dragonDamage())
+
+
+
+// function randomNumber(min, max) { 
+//   return Math.random() * (max - min) + min;
+// } 
+
+// https://www.geeksforgeeks.org/how-to-generate-random-number-in-given-range-using-javascript/
