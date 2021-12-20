e6ddbb74318bdcc49b87868b4941a19ce64a72db
Samnas <samucasnascimento@hotmail.com>
2021-12-20 16:55:53 -0300

    Practice-Exercises-Bonus Ex3

--- Fundamentos do Desenvolvimento Web/Bloco 8 - Higher Order Functions do JavaScript ES6/practice-exercises-Bonus.js
+++ Fundamentos do Desenvolvimento Web/Bloco 8 - Higher Order Functions do JavaScript ES6/practice-exercises-Bonus.js
@@ -37,5 +37,47 @@ const battleMembers = { mage, warrior, dragon };
 // 2 - Crie uma função que retorna o dano causado pelo warrior .
 // O dano será um número aleatório entre o valor do atributo strength (dano mínimo) e o valor de strength * weaponDmg (dano máximo).
 
-const warriorDamage = () => Math.random() * ((warrior.strength*2) - warrior.strength)  + warrior.strength;
+const warriorDamage = () => Math.random() * ((battleMembers.warrior.strength*2) - battleMembers.warrior.strength)  + battleMembers.warrior.strength;
 console.log(warriorDamage())
+
+
+
+// 3 - Crie uma função que retorna um objeto com duas chaves e dois valores contendo o dano e a mana gasta pelo mago em um turno.
+// O dano será um número aleatório entre o valor do atributo intelligence (dano mínimo) e o valor de intelligence * 2 (dano máximo).
+// A mana consumida por turno é 15. Além disto a função deve ter uma condicional, caso o mago tenha menos de 15 de mana o valor de dano recebe uma mensagem (Ex: "Não possui mana suficiente") e a mana gasta é 0.
+
+const mageDamage = () => {
+  const damageTotal = 
+  {
+    damage: chkMageDamage(),
+    mana: battleMembers.mage.mana -= spentMana()
+  }
+  return damageTotal;
+}
+
+const chkMageDamage = () => {
+  const damage = Math.random()*(mage.intelligence * 2 - mage.intelligence) + mage.intelligence;
+  return damage;
+}
+
+
+let statusMana = 0;
+
+const spentMana = () => {
+  if (mage.mana < 15) {
+    return 'Não possui mana suficiente';
+  }
+  else {
+    statusMana += 15;
+    return statusMana;
+  }
+}
+console.log(spentMana());
+
+console.log(mageDamage())
+console.log(mageDamage())
+console.log(mageDamage())
+console.log(mageDamage())
+console.log(mageDamage())
+console.log(mageDamage())
+
