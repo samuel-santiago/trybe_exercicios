cd665e91328d7226983e06afccafadef98efdd07
Samnas <samucasnascimento@hotmail.com>
2022-01-24 15:57:18 -0300

    Creating Footer Component

--- Desenvolvimento Front-end/Bloco 10 - Introdução à React/practice-exercises-10.1/practice-exercises-2/src/App.js
+++ Desenvolvimento Front-end/Bloco 10 - Introdução à React/practice-exercises-10.1/practice-exercises-2/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+import Content from './Content';
+
 
 function App() {
   return (
-   <Header></Header>
+    <div>
+      <Header> </Header>
+      <Content> </Content>
+    </div>
   );
 }
 
---
+++ Desenvolvimento Front-end/Bloco 10 - Introdução à React/practice-exercises-10.1/practice-exercises-2/src/Footer.jsx
@@ -0,0 +1,12 @@
+import react from "react";
+
+class Footer extends react.Component{
+  render(){
+    return (
+      
+
+    )
+  }
+
+
+}
