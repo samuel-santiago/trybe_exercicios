3ad6f33ea5d1acb30f5d9f61ba6e73cdd36d64ba
Samnas <samucasnascimento@hotmail.com>
2022-01-24 14:36:24 -0300

    Learning Exercise Complete

--- Desenvolvimento Front-end/learning-exercises-10.1/src/App.js
+++ Desenvolvimento Front-end/learning-exercises-10.1/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Component from './Component';
 
 function App() {
   return (
-);
+    <Component></Component>
+  )
 }
 
 export default App;
---
+++ Desenvolvimento Front-end/learning-exercises-10.1/src/Component.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Component extends React.Component {
+
+render() {
+  return (
+    <div>
+      <h1> Olá, meu nome é Samuel. </h1>
+      <p>\n Estou estudando na Trybe e \n estou muito feliz!</p>
+    </div>
+  )
+  }
+
+}
+
+export default Component
