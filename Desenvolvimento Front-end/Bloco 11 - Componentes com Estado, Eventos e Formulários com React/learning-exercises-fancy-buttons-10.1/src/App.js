9f2bb490549424bfe376ff9924ca27f8d5827024
Samnas <samucasnascimento@hotmail.com>
2022-01-26 21:46:30 -0300

    Learning-Exercises-Fancy-Buttons-2

--- Desenvolvimento Front-end/Bloco 11 - Componentes com Estado, Eventos e Formulários com React/learning-exercises-fancy-buttons-10.1/src/App.js
+++ Desenvolvimento Front-end/Bloco 11 - Componentes com Estado, Eventos e Formulários com React/learning-exercises-fancy-buttons-10.1/src/App.js
@@ -2,14 +2,31 @@ import react from 'react';
 import './App.css';
 
 const handleClick = () => {
-  console.log('Evento Clicado')
-} 
+  console.log('Evento Clicado1')
+}
+
+const handleClick2 = () => {
+  console.log('Evento Clicado2')
+}
+
+const handleClick3 = () => {
+  console.log('Evento Clicado3')
+}
+
 class App extends react.Component {
   render(){
     return (
-      <button onClick={handleClick}>
-        Botão
-      </button>
+      <>
+        <button key='botão1' onClick={handleClick}>
+          Botão1
+        </button>
+        <button key='botão2' onClick={handleClick2}>
+          Botão2
+        </button>
+        <button key='botão3' onClick={handleClick3}>
+          Botão3
+        </button>
+    </>
     )
   }
 }
