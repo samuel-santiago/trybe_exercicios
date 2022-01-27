8e4c2386b30974d3249fa655ce028a4f783e553e
Samnas <samucasnascimento@hotmail.com>
2022-01-26 22:48:39 -0300

    Learning-Exercises-Fancy-Buttons-4

--- Desenvolvimento Front-end/Bloco 11 - Componentes com Estado, Eventos e Formulários com React/learning-exercises-fancy-buttons-10.1/src/App.js
+++ Desenvolvimento Front-end/Bloco 11 - Componentes com Estado, Eventos e Formulários com React/learning-exercises-fancy-buttons-10.1/src/App.js
@@ -4,14 +4,13 @@ import './App.css';
 
 class App extends react.Component {
 
-  // constructor () {
-  //   super()
-  //   this.handleClick = this.handleClick.bind(this);
-  //   console.log('constructor: ',this)
-  // }
+  constructor () {
+    super()
+    this.handleClick = this.handleClick.bind(this);
+    console.log('constructor: ',this)
+  }
 
   handleClick = () => {
-    // Essa chamada ao `this` retorna `undefined`? !
     console.log(this)
     console.log('Clicou')
   }
@@ -32,19 +31,5 @@ class App extends react.Component {
     )
   }
 }
-
-  // class App extends react.Component {
-  //   handleClick () {
-  //     // Essa chamada ao `this` retorna `undefined`? !
-  //     console.log(this)
-  //     console.log('Clicou')
-  //   }
-  
-  //   render() {
-  //     // Já essa chamada ao `this`, feita de dentro da função `render`, retorna o objeto que esperamos
-  //     // console.log(this)
-  //     return <button onClick={this.handleClick}>Meu botão</button>
-  //   }
-  // }
 
 export default App;
