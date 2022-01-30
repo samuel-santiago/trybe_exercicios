a19ffd15f36594057d27bf8d9ae44eab246baa85
Samnas <samucasnascimento@hotmail.com>
2022-01-30 13:22:21 -0300

    Organizing Folders

--- Desenvolvimento Front-end/Bloco 11 - Componentes com Estado, Eventos e Formulários com React/learning-exercises-fancy-buttons-11.1/src/App.js
+++ Desenvolvimento Front-end/Bloco 11 - Componentes com Estado, Eventos e Formulários com React/learning-exercises-fancy-buttons-11.1/src/App.js
@@ -7,9 +7,9 @@ class App extends react.Component {
   constructor () {
     super()
 
-    this.handleClick1 = this.handleClick1.bind(this);
-    this.handleClick2 = this.handleClick2.bind(this);
-    this.handleClick3 = this.handleClick3.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    // this.handleClick2 = this.handleClick2.bind(this);
+    // this.handleClick3 = this.handleClick3.bind(this);
     // Observação, se eu tirar os 3 binds, não acontece nada...
 
     this.state = {
@@ -22,57 +22,56 @@ class App extends react.Component {
     return num % 2 === 0 ? 'green' : 'white';
   }
 
-  handleClick1 (string) {
-    this.setState(({Botão1}) => ({
-      Botão1: Botão1 +1,
+  handleClick (string) {
+    this.setState(({string}) => ({
+      [string]: string + 1,
     }), () => {
       console.log(`
       ${string} 
-      Count: ${this.state.Botão1} 
-      Color: ${this.getCollorButton(this.state.Botão1)}` )
-    });
-
-  }
-
-  handleClick2 (string) {
-    this.setState(({Botão2}) => ({
-      Botão2: Botão2 +1,
-    }), () => {
-      console.log(`
-      ${string} 
-      Count: ${this.state.Botão2} 
-      Color: ${this.getCollorButton(this.state.Botão2)}` )
+      Count: ${this.state[string]} 
+      Color: ${this.getCollorButton(this.state[string])}` )
     });
   }
 
-  handleClick3 (string) {
-    this.setState(({Botão3}) => ({
-      Botão3: Botão3 +1,
-    }), () => {
-      console.log(`
-      ${string} 
-      Count: ${this.state.Botão3} 
-      Color: ${this.getCollorButton(this.state.Botão3)}` )
-    });
-  }
+  // handleClick (string) {
+  //   this.setState(({Botão2}) => ({
+  //     Botão2: Botão2 +1,
+  //   }), () => {
+  //     console.log(`
+  //     ${string} 
+  //     Count: ${this.state.Botão2} 
+  //     Color: ${this.getCollorButton(this.state.Botão2)}` )
+  //   });
+  // }
+
+  // handleClick3 (string) {
+  //   this.setState(({Botão3}) => ({
+  //     Botão3: Botão3 +1,
+  //   }), () => {
+  //     console.log(`
+  //     ${string} 
+  //     Count: ${this.state.Botão3} 
+  //     Color: ${this.getCollorButton(this.state.Botão3)}` )
+  //   });
+  // }
 
   render(){
     return (
       <>
         <button key={'botão1'} 
           onClick={(evt) => {
-            this.handleClick1(evt.target.innerText)
+            this.handleClick(evt.target.innerText)
           }} 
           style={{backgroundColor: this.getCollorButton(this.state.Botão1)}}>
           Botão1
         </button>
         <button key='botão2' 
-          onClick={(evt) => this.handleClick2(evt.target.innerText)} 
+          onClick={(evt) => this.handleClick(evt.target.innerText)} 
           style={{backgroundColor: this.getCollorButton(this.state.Botão2)}}>
           Botão2
         </button>
         <button key='botão3' 
-          onClick={(evt) => this.handleClick3(evt.target.innerText)} 
+          onClick={(evt) => this.handleClick(evt.target.innerText)} 
           style={{backgroundColor: this.getCollorButton(this.state.Botão3)}}>
           Botão3
         </button>
