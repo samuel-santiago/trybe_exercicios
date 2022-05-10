781db87fa68a826248088a5bf7d62e86cb228b1f
Samnas <samucasnascimento@hotmail.com>
2022-05-09 22:48:00 -0300

    refactor: :hammer: use switch case to print BMI status considering the IMC calculated

--- Desenvolvimento Back-end/Bloco 22 - Introdução ao desenvolvimento Web com Node.js/hello-world/dia-1-nodejs-um-motor-javascript/practice-exercises-22.1/imc.js
+++ Desenvolvimento Back-end/Bloco 22 - Introdução ao desenvolvimento Web com Node.js/hello-world/dia-1-nodejs-um-motor-javascript/practice-exercises-22.1/imc.js
@@ -5,7 +5,33 @@ const calculateBMI = () => {
   let height = readline.questionFloat('What is your height?');
   height > 100 ? height = height / 100 : height;
   
-  const IMC = weight/ (height * height);
-  console.log(IMC.toFixed(2));
+  const IMC = weight/ (height ** 2);
+
+  printBMI_rating(IMC);
 }
+
+const printBMI_rating = (IMC) => {
+  switch (true) {
+    case IMC <= 18.5:
+      console.log('UnderWeight');
+      break;
+    case IMC >= 18.5 && IMC < 25:
+      console.log('Fit');
+      break;
+    case IMC >= 25 && IMC < 30:
+      console.log('Overweight')
+      break;
+    case IMC >= 30 && IMC < 35:
+      console.log('Obesity I');
+      break;
+    case 35 <= IMC <= 39.9:
+      console.log('Obesity II');
+      break;
+    case IMC >= 40:
+      console.log('Obesity IV & V');
+      break;
+    default:
+      break;
+  }
+} 
 calculateBMI();
