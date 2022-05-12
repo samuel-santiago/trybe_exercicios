65dfc7a83b98dc041cee9ac848db7c93893e24eb
Samnas <samucasnascimento@hotmail.com>
2022-05-11 21:50:48 -0300

    docs: :rocket: writting question statement ex1

--- Desenvolvimento Back-end/Bloco 22 - Introdução ao desenvolvimento Web com Node.js/dia-3-Mocha-chai-e-sinon-testes-de-back-end-com-node.js/practice-exercises-22.3/tests/ex1.test.js
+++ Desenvolvimento Back-end/Bloco 22 - Introdução ao desenvolvimento Web com Node.js/dia-3-Mocha-chai-e-sinon-testes-de-back-end-com-node.js/practice-exercises-22.3/tests/ex1.test.js
@@ -1,3 +1,10 @@
+// Exercício 1: Estruture os testes utilizando mocha e chai para um função que irá dizer se um número é "positivo", "negativo" ou "neutro":
+// Essa função irá receber um número como parâmetro e retornar uma string como resposta;
+// Quando o número passado for maior que 0 deverá retornar "positivo", quando for menor que 0 deverá retornar "negativo" e quando igual a 0 deverá retornar "neutro";
+// Descreva todos os cenário de teste utilizando describes;
+// Descreva todos os testes que serão feitos utilizando its;
+// Crie as asserções validando se os retornos de cada cenário tem o tipo e o valor esperado.
+
 const { expect } = require('chai');
 const {describe, it} = require('mocha');
 
